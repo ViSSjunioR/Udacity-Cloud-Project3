ba9125c5c371f8dfed2abafa1c5dad1169b5049d
vietth <vietth@Vietths-MacBook-Pro.local>
2023-07-03 10:47:17 +0700

    Add source project 3

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=vietth14
+export POSTGRES_PASSWORD=12345678
+export POSTGRES_HOST=database-2.cuubemjdjkjr.us-west-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=vietth14-project3-bucket
+export AWS_REGION=us-west-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
