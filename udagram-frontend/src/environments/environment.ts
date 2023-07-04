8aaa98a714ecda6a9c5ca3c5c616db56b4a4dd72
vietth <vietth@Vietths-MacBook-Pro.local>
2023-07-04 23:21:09 +0700

    add file build

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a5af19161627e4097b2629899d17b44e-2109728764.us-west-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
